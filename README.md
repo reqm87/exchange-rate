c0c11942d9a41710b15622a920d43ed6e73bfbb1
Rafael Quintero <rquintero@bitdharma.com>
2019-05-20 12:39:19 -0400

    Adding base code for Flask

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Exchange Rate
 
 ## Coding Challenge
-
--- core/main.py
+++ core/main.py
@@ -1,6 +1,13 @@
-def main():
-    print('Hello world!')
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def exchange_rate():
+    return "Exchange rate is working!"
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
