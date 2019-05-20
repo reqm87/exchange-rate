27117bb1d5455e452ad95332423e7e65e44af687
Rafael Quintero <rquintero@bitdharma.com>
2019-05-20 15:50:23 -0400

    Refactoring some code

--- core/provider.py
+++ core/provider.py
@@ -1,42 +1,21 @@
-import datetime
-
 import requests
-from bs4 import BeautifulSoup
+from serializer import banxico_format, dof_format, fixer_format
 from settings import BANXICO_API_TOKEN
 from urls import BANXICO_URL, DOF_URL, FIXER_URL
 
 
 def dof():
     response = requests.get(DOF_URL)
-    soup = BeautifulSoup(response.text, "html.parser")
-    row = soup.find('tr', attrs={'class': 'renglonNon'})
-    values = row.text.replace('\n', '').replace('\r', '').split(' ')
-    values = list(filter(None, values))
-    value = float(values[1])
-    last_updated = datetime.datetime.strptime(
-        values[0], '%d/%m/%Y'
-    ).isoformat()
-    result = {'dof': {'value': value, 'last_updated': last_updated}}
-    return result
+    return dof_format(response.text)
 
 
 def fixer():
-    data = requests.get(FIXER_URL).json()
-    value = 100 / data['rates']['USD'] / 100 * data['rates']['MXN']
-    last_updated = datetime.datetime.fromtimestamp(
-        data['timestamp']
-    ).isoformat()
-    result = {'fixer': {'value': value, 'last_updated': last_updated}}
-    return result
+    response = requests.get(FIXER_URL)
+    return fixer_format(response.json())
 
 
 def banxico():
-    data = requests.get(
+    response = requests.get(
         BANXICO_URL, headers={'Bmx-Token': BANXICO_API_TOKEN}
-    ).json()
-    value = float(data['bmx']['series'][0]['datos'][0]['dato'])
-    last_updated = datetime.datetime.strptime(
-        data['bmx']['series'][0]['datos'][0]['fecha'], '%d/%m/%Y'
-    ).isoformat()
-    result = {'banxico': {'value': value, 'last_updated': last_updated}}
-    return result
+    )
+    return banxico_format(response.json())
---
+++ core/serializer.py
@@ -0,0 +1,34 @@
+import datetime
+
+from bs4 import BeautifulSoup
+
+
+def dof_format(data):
+    soup = BeautifulSoup(data, "html.parser")
+    row = soup.find('tr', attrs={'class': 'renglonNon'})
+    values = row.text.replace('\n', '').replace('\r', '').split(' ')
+    values = list(filter(None, values))
+    value = float(values[1])
+    last_updated = datetime.datetime.strptime(
+        values[0], '%d/%m/%Y'
+    ).isoformat()
+    result = {'dof': {'value': value, 'last_updated': last_updated}}
+    return result
+
+
+def fixer_format(data):
+    value = 100 / data['rates']['USD'] / 100 * data['rates']['MXN']
+    last_updated = datetime.datetime.fromtimestamp(
+        data['timestamp']
+    ).isoformat()
+    result = {'fixer': {'value': value, 'last_updated': last_updated}}
+    return result
+
+
+def banxico_format(data):
+    value = float(data['bmx']['series'][0]['datos'][0]['dato'])
+    last_updated = datetime.datetime.strptime(
+        data['bmx']['series'][0]['datos'][0]['fecha'], '%d/%m/%Y'
+    ).isoformat()
+    result = {'banxico': {'value': value, 'last_updated': last_updated}}
+    return result
